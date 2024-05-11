fb23f16ad50125b46cc54d1fdc83096d6a7c2ba5
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2024-05-12 04:38:42 +0530

    Create Improve_taskbar win10-11 and remove widgets.py

---
+++ Disable/Improve_taskbar win10-11 and remove widgets.py
@@ -0,0 +1,20 @@
+import winreg
+
+def set_registry_value(key_path, value_name, value_data, value_type=winreg.REG_DWORD):
+    try:
+        key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, key_path)
+        winreg.SetValueEx(key, value_name, 0, value_type, value_data)
+        print(f"Registry value '{value_name}' set to '{value_data}' in '{key_path}'.")
+        winreg.CloseKey(key)
+    except Exception as e:
+        print("An error occurred:", e)
+
+# Windows 11 Taskbar Dashboard
+set_registry_value(r"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarDa", 0)
+
+# Windows 10 Taskbar Feeds
+set_registry_value(r"Software\Microsoft\Windows\CurrentVersion\Feeds", "ShellFeedsTaskbarViewMode", 2)
+
+# Disable Widgets service
+set_registry_value(r"SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests", "value", 0)
+set_registry_value(r"SOFTWARE\Policies\Microsoft\Dsh", "AllowNewsAndInterests", 0)
