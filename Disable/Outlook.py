c98360c997ae23b040f1aa8c79824cd34d3f3c95
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2024-05-12 03:48:54 +0530

    Outlook.py

---
+++ Disable/Outlook.py
@@ -0,0 +1,14 @@
+import winreg
+
+def set_registry_value(key_path, value_name, value_data, value_type=winreg.REG_DWORD):
+    try:
+        key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, key_path)
+        winreg.SetValueEx(key, value_name, 0, value_type, value_data)
+        print(f"Registry value '{value_name}' set to '{value_data}' in '{key_path}'.")
+        winreg.CloseKey(key)
+        print("OneDrive disabled successfully.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+# Disable OneDrive
+set_registry_value(r"SOFTWARE\Policies\Microsoft\Windows\OneDrive", "DisableFileSyncNGSC", 1)
